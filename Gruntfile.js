625c409b53acc5d8e9074c5b78ef5598e113b5d5
Hua Z <corona7@163.com>
2015-12-09 16:41:32 +0800

    add template files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,19 @@ module.exports = function (grunt) {
     });
     //加载提供"uglify"任务的插件
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-concat');
 
     //默认任务
     grunt.registerTask('default', ['uglify']);
+    //grunt.registerTask('default', ['concat', 'uglify']);
+
+    grunt.registerTask('foo', 'A sample task that logs stuff.', function(arg1, arg2) {
+        if(arguments.length === 0) {
+            grunt.log.writeln(this.name + ",没有参数");
+        } else if (arguments.length === 1) {
+            gurnt.log.writeln(this.name + ",arg1:" + arguments[0]);
+        } else {
+            grunt.log.writeln(this.name + ",arg1:" + arguments[0] + ",arg2:" + arguments[1]);
+        }
+    });
 }
